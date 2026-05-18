475e064375d5973c0f683151c1462dca01251d57
Robert Voong <rvoong1408@gmail.com>
2026-05-19 01:24:54 +0200

    metodo traductor para traduccion de palabras a morse

--- Traductor.java
+++ Traductor.java
@@ -7,6 +7,7 @@ public class Traductor {
     }
     private static HashMap<Character, String> diccionarioMorse = new HashMap<>();
     {
+        //Introducimos dentro del hashmap todas las llaves
     diccionarioMorse.put('A', ".-");
         diccionarioMorse.put('B', "-...");
         diccionarioMorse.put('C', "-.-.");
@@ -33,5 +34,42 @@ public class Traductor {
         diccionarioMorse.put('X', "-..-");
         diccionarioMorse.put('Y', "-.--");
         diccionarioMorse.put('Z', "--..");
+    }
+    public static String traductor(String palabra){
+        String resultado = "";
+        //Si la palabra no es valida no se ejecuta y se devuelve un string vacio
+        if(validString(palabra)){
+            StringBuilder provisional = new StringBuilder();
+            //Pasamos la palabra a mayuscula porque asi tenemos las llaves en el hashmap
+            String mayuscula = palabra.toUpperCase();
+            //Recorremos la palabra y vamos traduciendo por caracter
+            for(int i = 0; i < mayuscula.length(); i++){
+                //sacamos cada letra por posicion en el string
+                char letra = mayuscula.charAt(i);
+                //Si nuestro abecedario contiene la letra, que es una llave del hashmap entonces hacemos
+                //append del contenido de la llave que es el equivalente en morse
+                if(diccionarioMorse.containsKey(letra)){
+                    provisional.append(diccionarioMorse.get(letra));
+
+                    //Agregamos despues de cada caracter un espacio para diferenciar cada letra
+                    //para el final de cada palabra ya se agregara un espacio distinto(7 espacios segun morse pero demasiado)
+                    if(i < mayuscula.length() - 1){
+                        provisional.append("");
+                    }
+                }else{
+                    //Si no esta dentro de nuestro abecedario vamos a añadir un signo de interrogacion
+                    provisional.append("?");
+                    if(i < mayuscula.length() - 1){
+                        //Seguimos añadiendo un espacio para identificar que es una letra
+                        provisional.append("");
+                    }
+                }
+
+
+            }
+            resultado = provisional.toString();
+        }
+        
+        return resultado;
     }
 }
