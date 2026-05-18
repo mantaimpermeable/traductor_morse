86de202fa3b370844ab7782892246d80a8624b32
Robert Voong <rvoong1408@gmail.com>
2026-05-19 01:29:42 +0200

    deepsekeada para añadir nuevos simbolos al abecedario

--- Traductor.java
+++ Traductor.java
@@ -5,35 +5,65 @@ public class Traductor {
     public static boolean validString(String palabra){
         return (palabra != null && palabra != "");
     }
-    private static HashMap<Character, String> diccionarioMorse = new HashMap<>();
+    private static HashMap<Character, String> abecedario = new HashMap<>();
     {
         //Introducimos dentro del hashmap todas las llaves
-    diccionarioMorse.put('A', ".-");
-        diccionarioMorse.put('B', "-...");
-        diccionarioMorse.put('C', "-.-.");
-        diccionarioMorse.put('D', "-..");
-        diccionarioMorse.put('E', ".");
-        diccionarioMorse.put('F', "..-.");
-        diccionarioMorse.put('G', "--.");
-        diccionarioMorse.put('H', "....");
-        diccionarioMorse.put('I', "..");
-        diccionarioMorse.put('J', ".---");
-        diccionarioMorse.put('K', "-.-");
-        diccionarioMorse.put('L', ".-..");
-        diccionarioMorse.put('M', "--");
-        diccionarioMorse.put('N', "-.");
-        diccionarioMorse.put('O', "---");
-        diccionarioMorse.put('P', ".--.");
-        diccionarioMorse.put('Q', "--.-");
-        diccionarioMorse.put('R', ".-.");
-        diccionarioMorse.put('S', "...");
-        diccionarioMorse.put('T', "-");
-        diccionarioMorse.put('U', "..-");
-        diccionarioMorse.put('V', "...-");
-        diccionarioMorse.put('W', ".--");
-        diccionarioMorse.put('X', "-..-");
-        diccionarioMorse.put('Y', "-.--");
-        diccionarioMorse.put('Z', "--..");
+    abecedario.put('A', ".-");
+        abecedario.put('B', "-...");
+        abecedario.put('C', "-.-.");
+        abecedario.put('D', "-..");
+        abecedario.put('E', ".");
+        abecedario.put('F', "..-.");
+        abecedario.put('G', "--.");
+        abecedario.put('H', "....");
+        abecedario.put('I', "..");
+        abecedario.put('J', ".---");
+        abecedario.put('K', "-.-");
+        abecedario.put('L', ".-..");
+        abecedario.put('M', "--");
+        abecedario.put('N', "-.");
+        abecedario.put('O', "---");
+        abecedario.put('P', ".--.");
+        abecedario.put('Q', "--.-");
+        abecedario.put('R', ".-.");
+        abecedario.put('S', "...");
+        abecedario.put('T', "-");
+        abecedario.put('U', "..-");
+        abecedario.put('V', "...-");
+        abecedario.put('W', ".--");
+        abecedario.put('X', "-..-");
+        abecedario.put('Y', "-.--");
+        abecedario.put('Z', "--..");
+
+        abecedario.put('0', "-----");
+        abecedario.put('1', ".----");
+        abecedario.put('2', "..---");
+        abecedario.put('3', "...--");
+        abecedario.put('4', "....-");
+        abecedario.put('5', ".....");
+        abecedario.put('6', "-....");
+        abecedario.put('7', "--...");
+        abecedario.put('8', "---..");
+        abecedario.put('9', "----.");
+
+        abecedario.put(' ', "/");  // Espacio entre palabras
+        abecedario.put('.', ".-.-.-");
+        abecedario.put(',', "--..--");
+        abecedario.put('?', "..--..");
+        abecedario.put('!', "-.-.--");
+        abecedario.put(';', "-.-.-.");
+        abecedario.put(':', "---...");
+        abecedario.put('"', ".-..-.");
+        abecedario.put('\'', ".----.");
+        abecedario.put('(', "-.--.");
+        abecedario.put(')', "-.--.-");
+        abecedario.put('&', ".-...");
+        abecedario.put('@', ".--.-.");
+        abecedario.put('=', "-...-");
+        abecedario.put('+', ".-.-.");
+        abecedario.put('-', "-....-");
+        abecedario.put('_', "..--.-");
+        abecedario.put('/', "-..-.");
     }
     public static String traductor(String palabra){
         String resultado = "";
@@ -48,20 +78,20 @@ public class Traductor {
                 char letra = mayuscula.charAt(i);
                 //Si nuestro abecedario contiene la letra, que es una llave del hashmap entonces hacemos
                 //append del contenido de la llave que es el equivalente en morse
-                if(diccionarioMorse.containsKey(letra)){
-                    provisional.append(diccionarioMorse.get(letra));
+                if(abecedario.containsKey(letra)){
+                    provisional.append(abecedario.get(letra));
 
                     //Agregamos despues de cada caracter un espacio para diferenciar cada letra
                     //para el final de cada palabra ya se agregara un espacio distinto(7 espacios segun morse pero demasiado)
                     if(i < mayuscula.length() - 1){
-                        provisional.append("");
+                        provisional.append(" ");
                     }
                 }else{
                     //Si no esta dentro de nuestro abecedario vamos a añadir un signo de interrogacion
                     provisional.append("?");
                     if(i < mayuscula.length() - 1){
                         //Seguimos añadiendo un espacio para identificar que es una letra
-                        provisional.append("");
+                        provisional.append(" ");
                     }
                 }
 
