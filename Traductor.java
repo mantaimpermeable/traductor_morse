26255617d75c8a35c8c986dc94430b78fe7f4072
Robert Voong <rvoong1408@gmail.com>
2026-05-18 12:16:26 +0200

    Nueva estructura con hashMap y letras añadidas

--- Traductor.java
+++ Traductor.java
@@ -1,6 +1,37 @@
+import java.util.HashMap;
+
 public class Traductor {
     //clase donde se ejecutara toda la logica de la traduccion
     public static boolean validString(String palabra){
         return (palabra != null && palabra != "");
+    }
+    private static HashMap<Character, String> diccionarioMorse = new HashMap<>();
+    {
+    diccionarioMorse.put('A', ".-");
+        diccionarioMorse.put('B', "-...");
+        diccionarioMorse.put('C', "-.-.");
+        diccionarioMorse.put('D', "-..");
+        diccionarioMorse.put('E', ".");
+        diccionarioMorse.put('F', "..-.");
+        diccionarioMorse.put('G', "--.");
+        diccionarioMorse.put('H', "....");
+        diccionarioMorse.put('I', "..");
+        diccionarioMorse.put('J', ".---");
+        diccionarioMorse.put('K', "-.-");
+        diccionarioMorse.put('L', ".-..");
+        diccionarioMorse.put('M', "--");
+        diccionarioMorse.put('N', "-.");
+        diccionarioMorse.put('O', "---");
+        diccionarioMorse.put('P', ".--.");
+        diccionarioMorse.put('Q', "--.-");
+        diccionarioMorse.put('R', ".-.");
+        diccionarioMorse.put('S', "...");
+        diccionarioMorse.put('T', "-");
+        diccionarioMorse.put('U', "..-");
+        diccionarioMorse.put('V', "...-");
+        diccionarioMorse.put('W', ".--");
+        diccionarioMorse.put('X', "-..-");
+        diccionarioMorse.put('Y', "-.--");
+        diccionarioMorse.put('Z', "--..");
     }
 }
