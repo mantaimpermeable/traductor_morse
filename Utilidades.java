21fc6990c975ccc31383ea6f1aa88d57890c0783
Robert Voong <rvoong1408@gmail.com>
2026-05-18 11:00:46 +0200

    Utilidades en pausa hasta hacer abecedario

--- Utilidades.java
+++ Utilidades.java
@@ -1,3 +1,20 @@
+
+//Clase donde se encuentran metodos para el manejo de strings
 public class Utilidades {
-    //Clase donde se encuentran metodos para el manejo de strings
+    
+    public static String conversor(String palabra, Abecedario abecedario){
+       
+        if(validString(palabra)){
+            for(int i = 0; i < palabra.length(); i++){
+             char letra = palabra.charAt(i);
+             
+
+            }
+        }
+        return "";
+    }
+    public static boolean validString(String palabra){
+        return (palabra != null && palabra != "");
+    }
+    
 }
