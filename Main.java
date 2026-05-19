1c7587c922a9bb37d97dc9d7e48dcaf0f4e67b3a
Robert Voong <rvoong1408@gmail.com>
2026-05-19 02:00:04 +0200

    logica del traductor hecha y llamada en el main

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+//Aqui se ejecutar el programa y poco mas
 public class Main {
     public static void main(String[] args){
-        //Aqui se ejecutar el programa y poco mas
+        Scanner scanner = new Scanner(System.in);
+        Traductor.empezar(scanner);
     }
 }
--- Traductor.java
+++ Traductor.java
@@ -1,9 +1,10 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Traductor {
     //clase donde se ejecutara toda la logica de la traduccion
-    public static boolean validString(String palabra){
-        return (palabra != null && palabra != "");
+    public static boolean validString(String texto){
+        return (texto != null && texto != "");
     }
     private static HashMap<Character, String> abecedario = new HashMap<>();
     {
@@ -65,14 +66,14 @@ public class Traductor {
         abecedario.put('_', "..--.-");
         abecedario.put('/', "-..-.");
     }
-    public static String traductor(String palabra){
+    public static String traductor(String texto){
         String resultado = "";
-        //Si la palabra no es valida no se ejecuta y se devuelve un string vacio
-        if(validString(palabra)){
+        //Si la texto no es valida no se ejecuta y se devuelve un string vacio
+        if(validString(texto)){
             StringBuilder provisional = new StringBuilder();
-            //Pasamos la palabra a mayuscula porque asi tenemos las llaves en el hashmap
-            String mayuscula = palabra.toUpperCase();
-            //Recorremos la palabra y vamos traduciendo por caracter
+            //Pasamos la texto a mayuscula porque asi tenemos las llaves en el hashmap
+            String mayuscula = texto.toUpperCase();
+            //Recorremos la texto y vamos traduciendo por caracter
             for(int i = 0; i < mayuscula.length(); i++){
                 //sacamos cada letra por posicion en el string
                 char letra = mayuscula.charAt(i);
@@ -82,7 +83,7 @@ public class Traductor {
                     provisional.append(abecedario.get(letra));
 
                     //Agregamos despues de cada caracter un espacio para diferenciar cada letra
-                    //para el final de cada palabra ya se agregara un espacio distinto(7 espacios segun morse pero demasiado)
+                    //para el final de cada texto ya se agregara un espacio distinto(7 espacios segun morse pero demasiado)
                     if(i < mayuscula.length() - 1){
                         provisional.append(" ");
                     }
@@ -102,4 +103,41 @@ public class Traductor {
         
         return resultado;
     }
+
+
+    //Logica del traductor
+    public static void empezar (Scanner scanner){
+        //uso booleano para controlar el while por costumbre de clase
+        boolean jugar = true;
+
+        System.out.println("Bienvenido al traductor de codigo morse hecho a las 1:36 de un martes cualquiera");
+        System.out.println("Puedes traducir palabras que contengan letras de la A a la Z, del 0 al 9 y algunos simbolos");
+        System.out.println("Si algun caracter no esta dentro de nuestro abecedario se mostrara con un ? y posiblemente se añada en algun momento");
+        System.out.println("Para salir del programa escribe : kirkexiting");
+
+        while(jugar){
+            System.out.println("Comienza a meter texto para traducir:");
+            String texto = scanner.nextLine();
+
+            //Si se introduce el texto de salida se da un mensaje y se rompe el bucle terminando el programa
+            if(texto.equalsIgnoreCase("kirkexiting")){
+                System.out.println("Saliendo del programa");
+                jugar = false;
+            }
+
+            //Si la entrada de texto esta vacia repetimos el programa y pedimos texto
+            if(texto.trim().isEmpty()){
+                System.out.println("No me ralles e introduce texto");
+                //creo que primera vez que uso continuo
+                continue;
+            }
+
+            //Damos el texto original y la traduccion en morse
+            String morse = traductor(texto);
+            System.out.println("Texto original: " + texto);
+            System.out.println("Texto en morse: " + morse);
+        }
+        scanner.close();
+    }
 }
+
