6cbfacb7db6d3c07bd68bc725d5922016e6187a4
Robert Voong <rvoong1408@gmail.com>
2026-05-28 21:04:52 +0200

    metodo para rellenar vacios y formato en encuadrar

--- Salida.java
+++ Salida.java
@@ -2,6 +2,7 @@ public class Salida {
 
     //Metodo que maneja un texto y lo devuelve en parrafo separando por / y
     // y lo devuelve encuadrado
+    //En este metodo se han puesto bullet points y espacios adicionales pero no deberia de ser asi
     public String encuadrar (String mensaje){
 
         int longitud = 0;
@@ -19,24 +20,30 @@ public class Salida {
 
         //Modificamos el top para que tenga tantos guiones como la longitud que queremos
         //mas dos guionesnpara que no se choquen
-        for(int i = 0; i < longitud; i++){
+        for(int i = 0; i < longitud + 2; i++){
             tope += "-";
         }
         StringBuilder formato = new StringBuilder();
-        formato.append(tope+"\n\n");
+        formato.append(String.format("%s \n|%s   |\n", tope, fillVoids(" ", longitud)));
         for(int i = 0; i < texto.length; i++){
             String frase = texto[i];
-            int ocupacion = frase.length();
-            formato.append(String.format("| %s", frase));
-            while(ocupacion <= longitud){
-                formato.append(" ");
-                ocupacion++;
-            }
+            formato.append(String.format("|  ·%s", fillVoids(frase, longitud)));
+            
             formato.append("|\n");
         }
-        formato.append("\n" + tope);
+        formato.append(String.format("|%s   |\n%s", fillVoids(" ", longitud), tope));
 
 
         return formato.toString();
+    }
+
+    public String fillVoids(String texto, int end){
+       
+        int ocupacion = texto.length();
+        while(ocupacion <= end){
+            texto += " ";
+            ocupacion++;
+        }
+        return texto;
     }
 }
